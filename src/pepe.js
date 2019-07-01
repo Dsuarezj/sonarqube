1caa434ca1700d5b164f01c468ce67f3d1940243
Denisse Suarez <dsuarez@thoughtworks.com>
2019-07-01 11:48:55 -0500

    Adds duplication

--- src/pepe.js
+++ src/pepe.js
@@ -1,8 +1,16 @@
 var hola = (nombre) => `Hola ${nombre}`;
 var hola2 = (nombre) => `Hola ${nombre}`;
 var hola3 = (nombre) => `Hola ${nombre}`;
+var suma = (numero, numero2) => numero + numero2;
+var suma2 = (numero, numero2) => numero + numero2;
+var suma3 = (numero, numero2) => numero + numero2;
+var suma4 = (numero, numero2) => numero + numero2;
 module.exports = {
   hola,
   hola2,
   hola3,
+  suma,
+  suma2,
+  suma3,
+  suma4
 };
